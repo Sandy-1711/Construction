feb53bfe26fcedab8b7c43b6ac3b14adeedbc8b0
Sandy-1711 <sandy1711003@gmail.com>
2023-12-03 00:35:14 +0530

    60% responsive

--- src/(components)/Copyright/Copyright.js
+++ src/(components)/Copyright/Copyright.js
@@ -1,7 +1,10 @@
 import copycss from './Copyright.module.css'
 export default function Copyright() {
     return <div className={copycss.copyright}>
+
         <div className={copycss.copyLeft}>
+        </div>
+        <div className={copycss.wrapper}>
             <p>Copyright @2023 All rights reserved!</p>
         </div>
     </div>
